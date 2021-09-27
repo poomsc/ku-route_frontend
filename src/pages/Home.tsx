1e40e1f99b412038bf50a3f1d57852e286eb5006
bossbsj <bossllsj555@gmail.com>
2021-09-27 11:37:38 +0700

    commit home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -80,10 +80,15 @@ const HomePage = () => {
             {' '}
             ค้นหาชื่อวิชา / รหัสวิชาที่อยากรู้
           </p>
-          <Form style={{ paddingLeft: '22vw', paddingRight: '22vw' }}>
-            <div className="form-group shadow d-flex">
-              <input
-                id="home-search-block"
+          <Form style={{ paddingLeft: '7vw', paddingRight: '7vw' }}>
+            <div className="form-group shadow d-flex w-100">
+              <SMTDropdown
+                fluid
+                search
+                selection
+                options={subjects.slice(0, 10)}
+                onChange={handleOnSelectSubject}
+                onSearchChange={onSearchChange}
                 type="text"
                 className="form-control text-black"
                 style={{
